5e39339b1ab2f465083541dfbea1523a0974b9dd
Kohei Takahashi <flast@flast.jp>
2014-08-18 07:40:23 +0900

    Add test for compile failure.
      see also: 5f2560f d48040a
                https://svn.boost.org/trac/boost/ticket/9851
    
    Signed-off-by: Kohei Takahashi <flast@flast.jp>

---
+++ test/compile_fail/adaptor/dropped_while_concept.cpp
@@ -0,0 +1,36 @@
+// Boost.Range 2.0 Extension library
+// via PStade Oven Library
+//
+// Copyright Kohei Takahashi 2014.
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "mock_range.hpp"
+#include <boost/range/adaptor/dropped_while.hpp>
+
+namespace
+{
+
+struct always_true
+{
+    typedef bool result_type;
+
+    bool operator()(int, int) const
+    {
+        return true;
+    }
+};
+
+} // anonymous namespace
+
+int main(int, const char**)
+{
+    using boost::range::unit_test::mock_range;
+    using boost::adaptors::dropped_while;
+
+    // This next line should fail when Boost.Range concept checking is
+    // enabled since the adaptor takes at least a SinglePassRange.
+    return (mock_range<boost::incrementable_traversal_tag>() |
+                dropped_while(always_true())).front();
+}
